f25ab2b1b49307f2daaca42578871d8057a05b21
Ajin <ajinmla94@gmail.com>
2023-02-17 23:17:33 +0530

    csrf update

--- chinninambi/settings.py
+++ chinninambi/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = BASE_DIR / 'assets'
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangoweb-production.up.railway.app/'
+]
