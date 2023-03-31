7d62a5ef9e165e6a9bcf2245a2c1263eba3855da
Ajin <ajinmla94@gmail.com>
2023-03-31 07:18:07 +0530

    first commit

--- chinninambi/settings.py
+++ chinninambi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'brands',
     'index',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- index/admin.py
+++ index/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import contactform
+from .import models
+from import_export.admin import ImportExportActionModelAdmin
 # Register your models here.
+
 
 admin.site.register(contactform)
